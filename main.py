be2a0ed242f93187081430fe1c0685f0dda97d75
Lev <lev.l.baranov@gmail.com>
2020-04-18 23:46:33 +0500

    Выполнение 7.3

--- main.py
+++ main.py
@@ -2,21 +2,23 @@ from pprint import pprint
 from utils import compare, int_val
 from itertools import product
 
-ADDRESS_WORDS = {'дом','улица','живет'}
-NAME_WORDS    = {'имя','зовут','фамилия'}
-AGE_WORDS     = {'возраст', 'старше', 'младше'}
+ADDRESS_WORDS = {'дом','улица','живет', 'квартира'}
+NAME_WORDS    = {'имя','зовут','фамилия', 'отчество'}
+AGE_WORDS     = {'возраст', 'старше', 'младше', 'лет'} # лет - для ровного счёта
 
 class Person:
-    def __init__(self, name, age, address):
-        self.name, self.age, self.address = name, age, address
-        self.key = (name, address)
+    def __init__(self, first_name, middle_name, last_name, age, street, house, apartment):
+        self.first_name, self.middle_name, self.last_name = first_name, middle_name, last_name
+        self.street, self.house, self.apartment = street, house, apartment
+        self.age = age
+        self.key = (self.first_name, self.middle_name, self.last_name, self.street, self.house, self.apartment)
     
     def __repr__(self):
-        return "Person('%s',%s,'%s')" % (self.name, self.age, self.address)
+        return "Person('%s','%s','%s',%s,'%s','%s','%s')" % (self.first_name, self.middle_name, self.last_name, self.age, self.street, self.house, self.apartment)
     
     def __eq__(self, obj):
         if type(obj) == Person:
-            return (self.name, self.age, self.address) == (obj.name, obj.age, obj.address)
+            return (self.first_name, self.middle_name, self.last_name, self.age, self.street, self.house, self.apartment) == (obj.first_name, obj.middle_name, obj.last_name, obj.age, obj.street, obj.house, obj.apartment)
         elif type(obj) == str:
             return self.__fuzzy_compare(obj)
         else:
@@ -24,21 +26,34 @@ class Person:
         
     def __fuzzy_compare(self, query):
         def by_address(Q):
-            Q = Q - ADDRESS_WORDS
-            W = set(self.address.replace(',','').split())
-            rez = []
-            for a, b in product(Q, W):
-                rez += [(compare(a,b),a,b)]
-            return max(rez)[0]
-            
+            if 'улица' in Q or 'живет' in Q:
+                W = set(self.street.split())
+                rez = []
+                Q = Q - ADDRESS_WORDS
+                for a, b in product(Q, W):
+                    rez += [(compare(a,b),a,b)]
+                return max(rez)[0]
+            elif 'дом' in Q:
+                query_house = max([ int_val(q) for q in Q ])
+                return query_house == self.house
+            elif 'квартира' in Q:
+                query_apartment = max([ int_val(q) for q in Q ])
+                return query_apartment == self.apartment
+            return 0
         
         def by_name(Q):
-            Q = Q - NAME_WORDS
-            W = set(self.name.split())
+            if "имя" in Q:
+                W = set(self.first_name.split())
+            elif 'отчество' in Q:
+                W = set(self.middle_name.split())
+            elif 'фамилия' in Q:
+                W = set(self.last_name.split())
+            else:
+                W = set(self.first_name.split()).union(self.middle_name.split(), self.last_name.split())
             rez = []
+            Q = Q - NAME_WORDS
             for a, b in product(Q, W):
                 rez += [(compare(a,b),a,b)]
-
             return max(rez)[0]
         
         def by_age(Q):
@@ -58,10 +73,10 @@ class Person:
                 
         return score > 0.49
 
-lena = Person("Лена",   19, "Пушкина,  14, 115")
-oleg = Person("Олег",   34, "Ленскoго, 10,  11")
-olga = Person("Ольга",  28, "Онегина,  11,  12")
-nata = Person("Наташа", 17, "Ростова,  16,  15")
+lena = Person("Елена",   "Ивановна",  "Воронова",  19, "Пушкина",  14, 115)
+oleg = Person("Олег",    "Петрович",  "Синицын",   34, "Ленскoго", 10, 11)
+olga = Person("Ольга",   "Фёдоровна", "Воробей",   28, "Онегина",  11, 12)
+nata = Person("Наталья", "Игоревна",  "Кукушкина", 17, "Ростова",  16, 15)
 
 queries = [
     'имя Ольга', 
@@ -71,7 +86,7 @@ queries = [
     'живет на Пушкина', 
     'дом 10',
     'фамилия ростова',
-    'зовут нташа',
+    'зовут Лена',
 ]
 
 people = {
