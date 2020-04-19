885b73c2389afa21a39e4a699afd37eb2ffd5017
Lev <lev.l.baranov@gmail.com>
2020-04-19 15:07:53 +0500

    Замена табов на пробелы

--- hw9.py
+++ hw9.py
@@ -2,7 +2,7 @@ from random import choice, randint
 import difflib
 
 def give_answer():
-	return choice(['Да', 'Нет'])
+    return choice(['Да', 'Нет'])
 
 def is_same(s1, s2):
     normalized1 = s1.lower()
@@ -14,22 +14,22 @@ def is_same(s1, s2):
         return False      
 
 def get_key_answer(q, list_key):
-	for q in list_key:
-		if is_same(question, q):
-			return questions[q]
-	return False
+    for q in list_key:
+        if is_same(question, q):
+            return questions[q]
+    return False
 
 questions = dict()
 print("Я отвечу только 'Да' или 'Нет'!")
 i = randint(2, 10)
 while (i > 0):
-	i -= 1
-	question = input("Задай свой вопрос: ")
-	if get_key_answer(question, questions.keys()):
-		answer = get_key_answer(question, questions.keys())
-	else:
-		answer = give_answer()
-		questions[question] = answer
-	print(answer)
+    i -= 1
+    question = input("Задай свой вопрос: ")
+    if get_key_answer(question, questions.keys()):
+        answer = get_key_answer(question, questions.keys())
+    else:
+        answer = give_answer()
+        questions[question] = answer
+    print(answer)
 
 print("Я устал отвечать на глупые вопросы! Уходи!")
