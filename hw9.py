3eb38b4b1ed256191e4a9f4c1732d3e47d901840
Lev <lev.l.baranov@gmail.com>
2020-04-19 14:42:06 +0500

    Выполнено задание 9.3

--- hw9.py
+++ hw9.py
@@ -1,7 +1,23 @@
 from random import choice, randint
+import difflib
 
 def give_answer():
 	return choice(['Да', 'Нет'])
+
+def is_same(s1, s2):
+    normalized1 = s1.lower()
+    normalized2 = s2.lower()
+    matcher = difflib.SequenceMatcher(None, normalized1, normalized2)
+    if matcher.ratio() > 0.67:
+        return True
+    else:
+        return False      
+
+def get_key_answer(q, list_key):
+	for q in list_key:
+		if is_same(question, q):
+			return questions[q]
+	return False
 
 questions = dict()
 print("Я отвечу только 'Да' или 'Нет'!")
@@ -9,8 +25,8 @@ i = randint(2, 10)
 while (i > 0):
 	i -= 1
 	question = input("Задай свой вопрос: ")
-	if question in questions.keys():
-		answer = questions[question]
+	if get_key_answer(question, questions.keys()):
+		answer = get_key_answer(question, questions.keys())
 	else:
 		answer = give_answer()
 		questions[question] = answer
