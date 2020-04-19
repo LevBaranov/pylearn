5b71aa2eb7240ee5d9629f11420a8c7c188f5376
Lev <lev.l.baranov@gmail.com>
2020-04-19 13:00:28 +0500

    Выполнено задание 9.2

--- hw9.py
+++ hw9.py
@@ -3,11 +3,17 @@ from random import choice, randint
 def give_answer():
 	return choice(['Да', 'Нет'])
 
+questions = dict()
 print("Я отвечу только 'Да' или 'Нет'!")
 i = randint(2, 10)
 while (i > 0):
 	i -= 1
-	input("Задай свой вопрос: ")
-	print(give_answer())
+	question = input("Задай свой вопрос: ")
+	if question in questions.keys():
+		answer = questions[question]
+	else:
+		answer = give_answer()
+		questions[question] = answer
+	print(answer)
 
 print("Я устал отвечать на глупые вопросы! Уходи!")
