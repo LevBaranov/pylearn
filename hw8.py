bd982ac7aba2c91f6fb484974502577ba6b4a6c1
Lev <lev.l.baranov@gmail.com>
2020-04-19 12:06:37 +0500

    Задание 8 после правок

--- hw8.py
+++ hw8.py
@@ -1,11 +1,64 @@
 from pprint import pprint
-import difflib # для нечёткого сравнения
+import difflib
+
+SEARCH_WORDS = {
+    'name': ['name', 'fname', 'nme', 'moniker'],
+    'role': ['role', 'character', 'function', 'roe'],
+    'power':['power', 'pwer', 'force', 'might']
+}
+WORDS_OPERATORS = {
+    'more': ['more', 'mo', 'mre', 'up', 'over'],
+    'less': ['less', 'ls', 'les', 'under', 'down']
+}
+
 class Person:
+
     def __init__(self, name, year_birth, role, skill, power):
-        self.name, self.year_birth, self.role, self.skill, self.power = name, year_birth, role, skill, power
+        self.name, self.year_birth, self.role = name, year_birth, role
+        self.skill, self.power = skill, power
         self.key = (name, year_birth, role)
+
     def __repr__(self):
-        return "Person('%s',%s,'%s','%s',%s)" % (self.name, self.year_birth, self.role, self.skill, self.power)
+        return "Person('%s',%s,'%s','%s',%s)" % (
+            self.name, self.year_birth, self.role, self.skill, self.power
+            )
+
+
+def search_prop(s, d):
+    list_words = s.lower().split();
+    for word in list_words:
+        for item in d:
+            if word in d[item]:
+                return item
+
+def is_same(s1, s2):
+    normalized1 = s1.lower()
+    normalized2 = s2.lower()
+    matcher = difflib.SequenceMatcher(None, normalized1, normalized2)
+    if matcher.ratio() > 0.67:
+        return True
+    else:
+        return False
+
+def search_key_dict(s, prop):
+    list_search_words = s.lower().split();
+    result = ''
+    for pers_key in persons:
+        for word in list_search_words:
+            if prop == 'name' and is_same(word, persons[pers_key].name):
+                result += str(pers_key)
+            elif prop == 'role' and is_same(word, persons[pers_key].role):
+                result += str(pers_key)
+            elif prop =='power' and word.isdigit():
+                power_num = int(word)
+                operator = search_prop(s, WORDS_OPERATORS)
+                if operator == 'more' and power_num < persons[pers_key].power + 2:
+                    result += str(pers_key)
+                elif operator == 'less' and power_num > persons[pers_key].power - 2:
+                    result += str(pers_key)
+                elif power_num in range(persons[pers_key].power - 2, persons[pers_key].power + 2):
+                    result += str(pers_key)
+    return result
 
 merlin = Person('Merlin', 450, 'wizard', 'wiseacre', 99)
 arthur = Person('Arthur', 472, 'king', 'knight', 83)
@@ -16,60 +69,11 @@ persons = {
     arthur.key: arthur,
     morgan.key: morgan,
 }
-#Добавим словарь с возможными ключами поиска
-dict_keys = {
-    'name': ['name', 'fname', 'nme', 'moniker'],
-    'role': ['role', 'character', 'function', 'roe'],
-    'power':['power', 'pwer', 'force', 'might']
-}
-#Добавим словарь с операторами сравнения
-dict_operators = {
-    'more': ['more', 'mo', 'mre', 'up', 'over'],
-    'less': ['less', 'ls', 'les', 'under', 'down']
-}
-def search_prop(s, d):
-    #Пробегаем по словарю с ключами и находим свойство по которому будет поиск. Находим только первое вхождение
-    list_search_string = s.lower().split();
-    for word in list_search_string:
-        for item in d:
-            if word in d[item]:
-                return item
-def similarity(s1, s2):
-    #С помощью библиотеки difflib будем искать неточные совпдания слов
-    normalized1 = s1.lower()
-    normalized2 = s2.lower()
-    matcher = difflib.SequenceMatcher(None, normalized1, normalized2)
-    if matcher.ratio() > 0.67: # параметр сходства слов
-        return True
-    else:
-        return False
-def search_key_dict(s, prop):
-    #Не много улучшил функцию. Уменьшил кол-во циклов.
-    list_search_string = s.lower().split();
-    result = ''
-    for item in persons:
-        for word in list_search_string:
-            if prop == 'name' and similarity(word, persons[item].name):
-                result += str(item)
-            elif prop == 'role' and similarity(word, persons[item].role):
-                result += str(item)
-            elif prop =='power' and word.isdigit():
-                powernum = int(word)
-                operator = search_prop(s, dict_operators)
-                #Предоставим право на ошибку на пару пунктов силы
-                if operator == 'more' and powernum < persons[item].power + 2:
-                    result += str(item)
-                elif operator == 'less' and powernum > persons[item].power - 2:
-                    result += str(item)
-                elif powernum in range(persons[item].power - 2, persons[item].power + 2):
-                    result += str(item)
-    return result
 
-#Три запроса на проверку
-qu1 = 'nme Arhur mrlin'
-qu2 = 'function kng'
-qu3 = 'power les 81'
+query1 = 'nme Arhur mrlin'
+query2 = 'function kng'
+query3 = 'power les 81'
 
-pprint(search_key_dict(qu1, search_prop(qu1, dict_keys)))
-pprint(search_key_dict(qu2, search_prop(qu2, dict_keys)))
-pprint(search_key_dict(qu3, search_prop(qu3, dict_keys)))
+pprint(search_key_dict(query1, search_prop(query1, SEARCH_WORDS)))
+pprint(search_key_dict(query2, search_prop(query2, SEARCH_WORDS)))
+pprint(search_key_dict(query3, search_prop(query3, SEARCH_WORDS)))
